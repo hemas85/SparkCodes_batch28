77e6ef76cf07e63e7c904ebaef2db134421579ca
User <User@DESKTOP-OK7BAEV>
2021-06-22 23:45:57 +0530

    Added the explode

--- new_test1/src/main/scala/new_pckg/new_obj.scala
+++ new_test1/src/main/scala/new_pckg/new_obj.scala
@@ -1,10 +1,12 @@
 package new_pckg
 
 import org.apache.spark._
-		import org.apache.spark.sql._
-	import org.apache.spark.sql.types._
-		import org.apache.spark.sql.Row
-		import org.apache.spark.sql.functions._
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.functions._
+import scala.io.Source
+
 
 object new_obj {
   
@@ -17,5 +19,29 @@ object new_obj {
 		
     val spark = SparkSession.builder().getOrCreate()
 		import spark.implicits._
+		
+		val html = Source.fromURL("https://randomuser.me/api/0.8/?results=1")
+val s = html.mkString
+//println(s)
+
+val urlrdd= sc.parallelize(List(s))
+
+val urldf = spark.read.json(urlrdd)
+
+urldf.show()
+urldf.printSchema()
+
+val df1=urldf
+.withColumn("results",explode(col("results")))
+.select("nationality","seed","version","results.user.username",
+    "results.user.cell","results.user.dob","results.user.email",
+    "results.user.gender","results.user.location.city","results.user.location.state",
+    "results.user.location.street","results.user.location.zip","results.user.md5","results.user.name.first",
+    "results.user.name.last","results.user.name.title","results.user.password","results.user.phone",
+    "results.user.picture.large","results.user.picture.medium","results.user.picture.thumbnail",
+    "results.user.registered","results.user.salt","results.user.sha1","results.user.sha256")
+    
+    df1.show()
+
   }
 }
